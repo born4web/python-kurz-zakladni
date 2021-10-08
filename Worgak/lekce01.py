edbabdbc361975d820389e692bbca0d337e5d519
turna <misobis@gmail.com>
2021-10-08 10:19:10 +0200

    Lekcia 1 vyskuane

---
+++ Worgak/lekce01.py
@@ -0,0 +1,22 @@
+print("ahoj")
+
+jmeno = "Michal"
+prijmeni = "Turna"
+vek = 37
+vyska = 170
+plat = "10 EUR/hod"
+print(f"Priezvisko: {prijmeni}, Meno: {jmeno}, Vek: {vek}, Plat: {plat}")
+
+skuska
+
+plat = 10
+mena = "EUR/hod"
+print("Moja mzda je: {0} {1}".format(plat, mena))
+
+skuska príklady
+
+aktualny_stav_plynomeru= 27485.604
+stav_plynomeru_predtým= 27460.873
+aktualny_stav_plynomeru-stav_plynomeru_predtým
+
+3.6/2
